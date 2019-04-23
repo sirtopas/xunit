9e45879caa34e115d3c1e912eba64d5f349ee048
James <sirtopas@gmail.com>
2019-04-23 21:25:48 +0100

    Added new type of tests - theories

---
+++ Theories.cs
@@ -0,0 +1,22 @@
+using Xunit;
+
+namespace unit_testing
+{
+    // Theories are tests which are only true for a particular set of data.
+    public class Theories
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
